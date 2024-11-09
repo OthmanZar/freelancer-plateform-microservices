fba64e368afd322d6bd3c718dc2e1f3b3e4afc99
Othman Zarrouk <othmanzarrouk30@gmail.com>
2024-11-09 19:28:06 +0100

    New things in category

---
+++ category-service/src/main/java/com/freelance/category/handler/GlobalExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.freelance.category.handler;
+
+import com.freelance.category.entities.SubCategory;
+import com.freelance.category.exceptions.CategoryException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CategoryException.class)
+    public ResponseEntity<String> handle(CategoryException exp) {
+        return ResponseEntity
+                .status(BAD_REQUEST)
+                .body(exp.getMessage());
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exp) {
+        var errors = new HashMap<String, String>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    var fieldName = ((FieldError) error).getField();
+                    var errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+
+        return ResponseEntity
+                .status(BAD_REQUEST)
+                .body(new ErrorResponse(errors));
+    }
+
+
+
+}
