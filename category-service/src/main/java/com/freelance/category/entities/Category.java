151030be1557e6be4208c36f4061cd4a019c95ce
Othman Zarrouk <othmanzarrouk30@gmail.com>
2024-11-09 03:09:30 +0100

    New things in category

---
+++ category-service/src/main/java/com/freelance/category/entities/Category.java
@@ -0,0 +1,29 @@
+package com.freelance.category.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String categoryName;
+
+
+    @OneToMany(mappedBy = "category",fetch = FetchType.EAGER)
+    private List<SubCategory> subCategoryList;
+
+}
---
+++ category-service/src/main/java/com/freelance/category/entities/SubCategory.java
@@ -0,0 +1,23 @@
+package com.freelance.category.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SubCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    private Category category;
+}
